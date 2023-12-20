6903f713e714e50c1e93bf0a8e5c1df1e525b79c
Ankit Prasad <ankit.prasad@interactive12.com>
2023-12-20 10:21:39 +0530

    Add project files.

---
+++ mvcloginregistration/Models/UserAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLoginRegistration.Models
+{
+    public class UserAccount
+    {
+
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password confirmation is required.")]
+        [DataType(DataType.Password)]
+        public string Confirmpassword { get; set; }
+
+    }
+}
